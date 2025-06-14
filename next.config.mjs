d1bebf2f9b044b4a976a622d28bc214eb39f7b38
Gregor Doroschenko <g.doroschenko@gmail.com>
2025-06-14 21:22:46 +0200

    Prepare next for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
